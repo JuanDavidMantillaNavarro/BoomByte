aaffa02845c4cf1d52110aa009352c86d653ac49
Nikol Sofia Forero Borja <est.nikol.forero@unimilitar.edu.co>
2026-03-26 11:41:33 -0500

    cambio para que desaparezca el letrero de lanzar bola

--- Assets/Scripts/LetrerosController.cs
+++ Assets/Scripts/LetrerosController.cs
@@ -1,20 +1,21 @@
-
 using System.Collections;
 using UnityEngine;
 
 public class LetrerosController : MonoBehaviour
 {
-     [Header("Imagenes de tutorial")]
+    [Header("Imagenes de tutorial")]
     public GameObject imagenAgarrarBola;
     public GameObject imagenLanzarBola;
 
     [Header("Prueba manual")]
     public bool agarrarBola = false;
+    public bool lanzarBola = false; 
 
     [Header("Tiempo de espera")]
     public float tiempoEspera = 5f;
 
     private bool cambioEjecutado = false;
+    private bool desaparicionEjecutada = false;
 
     void Start()
     {
@@ -27,10 +28,18 @@ public class LetrerosController : MonoBehaviour
 
     void Update()
     {
+        // Cambio a "Lanzar bola"
         if (agarrarBola && !cambioEjecutado)
         {
             cambioEjecutado = true;
             StartCoroutine(CambiarImagenTutorial());
+        }
+
+        //  Desaparición del letrero "Lanzar bola"
+        if (lanzarBola && !desaparicionEjecutada)
+        {
+            desaparicionEjecutada = true;
+            StartCoroutine(DesaparecerLetreroLanzar());
         }
     }
 
@@ -43,5 +52,13 @@ public class LetrerosController : MonoBehaviour
 
         if (imagenLanzarBola != null)
             imagenLanzarBola.SetActive(true);
+    }
+
+    IEnumerator DesaparecerLetreroLanzar()
+    {
+        yield return new WaitForSeconds(tiempoEspera);
+
+        if (imagenLanzarBola != null)
+            imagenLanzarBola.SetActive(false);
     }
 }
