dbe1c23d9e694f1738e39a47ddadcb99b8a0c9d7
LymoEm <est.emily.mora@unimilitar.edu.co>
2026-05-31 20:27:06 -0500

    mejoras menu sonidos control

---
+++ Assets/Scripts/AudioManagerFMODVR.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using FMODUnity;
+using FMOD.Studio;
+
+public class AudioManagerFMODVR : MonoBehaviour
+{
+    [Header("Sliders")]
+    public Slider sliderGeneral;
+    public Slider sliderMusica;
+    public Slider sliderEfectos;
+
+    private Bus masterBus;
+    private Bus musicBus;
+    private Bus sfxBus;
+
+    private void Start()
+    {
+        masterBus = RuntimeManager.GetBus("bus:/");
+        musicBus = RuntimeManager.GetBus("bus:/Music");
+        sfxBus = RuntimeManager.GetBus("bus:/SFX");
+
+        sliderGeneral.onValueChanged.AddListener(SetGeneral);
+        sliderMusica.onValueChanged.AddListener(SetMusica);
+        sliderEfectos.onValueChanged.AddListener(SetEfectos);
+
+        sliderGeneral.value =
+            PlayerPrefs.GetFloat("MasterVol", 1f);
+
+        sliderMusica.value =
+            PlayerPrefs.GetFloat("MusicVol", 1f);
+
+        sliderEfectos.value =
+            PlayerPrefs.GetFloat("SFXVol", 1f);
+
+        SetGeneral(sliderGeneral.value);
+        SetMusica(sliderMusica.value);
+        SetEfectos(sliderEfectos.value);
+    }
+
+    public void SetGeneral(float value)
+    {
+        masterBus.setVolume(value);
+        PlayerPrefs.SetFloat("MasterVol", value);
+    }
+
+    public void SetMusica(float value)
+    {
+        musicBus.setVolume(value);
+        PlayerPrefs.SetFloat("MusicVol", value);
+    }
+
+    public void SetEfectos(float value)
+    {
+        sfxBus.setVolume(value);
+        PlayerPrefs.SetFloat("SFXVol", value);
+    }
+}
